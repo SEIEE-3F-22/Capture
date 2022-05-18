c6a6a9cbb8904f33443b8c0a3651793a088c6e4a
Polaris <1223089722@qq.com>
2022-05-18 13:52:58 +0800

    Update Openvino interface

--- Capture.h
+++ Capture.h
@@ -3,11 +3,15 @@
 
 #include <iostream>
 #include <fstream>
+#include <condition_variable>
+#include <thread>
+#include <mutex>
+#include <utility>
 
 #include <opencv2/opencv.hpp>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
-#include <condition_variable>
+#include <pybind11/stl.h>
 
 #include "Openvino.h"
 
@@ -23,27 +27,29 @@ public:
 
     void Undistort();
 
-    py::array_t<unsigned char> getCorrected();
+    py::array_t<unsigned char> getImage();
 
-    void OpenvinoInference();
+    void beginInference();
+
+    std::vector<Object> getInferenceResult();
 
 private:
     cv::VideoCapture cap;
     bool newFrameReceived = false;
     bool newFrameFinished = false;
     cv::Mat mapx, mapy;
-    cv::Mat frame, corrected;
+    cv::Mat frame, image;
 
-    std::mutex frame_mutex, corrected_mutex;
-    std::condition_variable cv_frameReceived;
+    std::mutex frame_mutex, image_mutex;
+    std::condition_variable frameReceived;
 
     CNNNetwork network;
-    std::vector<Object> objects;
+    std::vector<Object> resultObjects;
     InferRequest infer_request;
     std::string input_name;
     std::string output_name;
 
-    void OpenvinoInit(std::string input_model_path, std::string device_name);
+    void initNetwork(std::string input_model_path, std::string device_name);
 
     static py::array_t<unsigned char> Mat2ndarray(const cv::Mat& src);
 };
